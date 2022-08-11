8564e88788d1b1b77dfeceba023640f5338ad569
karolisjon <karolisjonn@gmail.com>
2022-08-11 16:44:33 +0300

    feat(14): created handleSubmit function which logs values entered in the form

--- react-crud-app/src/components/add-product-form.jsx
+++ react-crud-app/src/components/add-product-form.jsx
@@ -30,71 +30,87 @@ const AddProductForm = () => {
   const [price, setPrice] = React.useState('');
   const [description, setDescription] = React.useState('');
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log({
+      title,
+      category,
+      imageURL,
+      price,
+      description,
+    });
+  };
+
   return (
     <Container maxWidth="xl" sx={{ px: 4, pt: 4 }}>
-      <Typography variant="h4" component="h1">New product details</Typography>
-      <Box sx={{
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        mb: 2,
-      }}
-      >
-        <TextField
-          label="Title"
-          variant="standard"
-          fullWidth
-          value={title}
-          onChange={(event) => setTitle(event.target.value)}
-        />
-        <TextField
-          id="standard-select-currency"
-          select
-          label="Category"
-          value={category}
-          onChange={(event) => setCategory(event.target.value)}
-          variant="standard"
-          fullWidth
+      <Box component="form" onSubmit={handleSubmit}>
+        <Typography variant="h4" component="h1">New product details</Typography>
+        <Box sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          mb: 2,
+        }}
         >
-          {categories.map(({ id, label }) => (
-            <MenuItem key={id} value={id}>
-              {label}
-            </MenuItem>
-          ))}
-        </TextField>
+          <TextField
+            label="Title"
+            variant="standard"
+            fullWidth
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+          <TextField
+            id="standard-select-currency"
+            select
+            label="Category"
+            value={category}
+            onChange={(event) => setCategory(event.target.value)}
+            variant="standard"
+            fullWidth
+          >
+            {categories.map(({ id, label }) => (
+              <MenuItem key={id} value={id}>
+                {label}
+              </MenuItem>
+            ))}
+          </TextField>
 
-        <TextField
-          label="Image URL"
-          variant="standard"
-          fullWidth
-          value={imageURL}
-          onChange={(event) => setImageURL(event.target.value)}
-        />
-        <TextField
-          label="Price"
-          variant="standard"
-          fullWidth
-          value={price}
-          onChange={(event) => setPrice(event.target.value)}
-        />
+          <TextField
+            label="Image URL"
+            variant="standard"
+            fullWidth
+            value={imageURL}
+            onChange={(event) => setImageURL(event.target.value)}
+          />
+          <TextField
+            label="Price"
+            variant="standard"
+            fullWidth
+            value={price}
+            onChange={(event) => setPrice(event.target.value)}
+          />
+        </Box>
+        <Box sx={{ mb: 2 }}>
+          <TextField
+            label="Description"
+            variant="standard"
+            multiline
+            fullWidth
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+          />
+        </Box>
+        <Box>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            sx={{ borderRadius: 0 }}
+          >
+            Add new product
+          </Button>
+        </Box>
       </Box>
-      <Box sx={{ mb: 2 }}>
-        <TextField
-          label="Description"
-          variant="standard"
-          multiline
-          fullWidth
-          value={description}
-          onChange={(event) => setDescription(event.target.value)}
-        />
-      </Box>
-      <Button
-        variant="contained"
-        color="primary"
-        sx={{ borderRadius: 0 }}
-      >
-        Add new product
-      </Button>
     </Container>
   );
 };
