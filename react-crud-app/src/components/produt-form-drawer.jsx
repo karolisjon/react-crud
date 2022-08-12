ba93f2727b6ad9685e5cabcf677228453090b4ee
karolisjon <karolisjonn@gmail.com>
2022-08-12 15:07:50 +0300

    feat(27): adjusted handleSubmit function to close the drawer when the product is created

--- react-crud-app/src/components/produt-form-drawer.jsx
+++ react-crud-app/src/components/produt-form-drawer.jsx
@@ -38,6 +38,8 @@ const ProductFormDrawer = ({ onSubmit }) => {
     setPrice('');
     setDescription('');
     setCategoriesOption([]);
+
+    setFormDrawerOpen(false);
   };
 
   React.useEffect(() => {
